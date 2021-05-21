4eeb50f85085dfa7cead2a1748e9f07884adeb31
Bharati <bharatisharada@gmail.com>
2021-05-21 22:42:31 +0530

    Added dependencies and starter packages

---
+++ app.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk');
+const readLineSync = require('readline-sync');
+const log = console.log;
+
+log(chalk.bgBlue.white.bold.underline("HARRY POTTER TRIVIA QUIZ"));
+
+// const user = readLineSync.question('Hi there1')
